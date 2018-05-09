b2d99aac3b766b5c301f6951b2b229006ca4e32a
lvzhi <lvzhi1988@126.com>
2018-05-09 18:56:36 +0800

    配置信息

--- src/main/java/com/comic/www/config/AppConfig.java
+++ src/main/java/com/comic/www/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.comic.www.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
@@ -12,7 +11,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.service.ApiInfo;
 
 import java.util.ArrayList;
